b594d25acb8d446e35d94b7ef7d24d54cc4f3392
Utku Anil Saykara <ge79quz@mytum.de>
2021-08-15 22:55:54 +0200

    Image to binary map translation function and test added

--- src/utilty.py
+++ src/utilty.py
@@ -11,3 +11,10 @@ def create_image_from_nparray(array, file_name, is_rgb):
         Image.fromarray(array, "RGB").save(file_name)
     else:
         Image.fromarray(array, "L").save(file_name)
+
+
+def rgb_to_binary(array):
+    binary_map = np.dot(array[..., :3], [0.2989, 0.5870, 0.1140])
+    binary_map[binary_map < 128] = 1
+    binary_map[binary_map >= 128] = 0
+    return binary_map
--- test/test.py
+++ test/test.py
@@ -13,6 +13,14 @@ def test_save_image(image):
     util.create_image_from_nparray(image, "/home/utku/Documents/3_new.png", True)
 
 
+def test_binary_map(image):
+    binary_map = util.rgb_to_binary(image)
+    print("Binary type: ", type(binary_map))
+    print("Binary shape: ", binary_map.shape)
+    print(binary_map)
+
+
 if __name__ == "__main__":
     img = test_open_image()
     test_save_image(img)
+    test_binary_map(img)
