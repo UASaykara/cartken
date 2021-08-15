ccaeec5e0f9b19a1ca4a70c08693b9ff987aa6d5
Utku Anil Saykara <ge79quz@mytum.de>
2021-08-15 22:32:21 +0200

    Image save function added

--- src/utilty.py
+++ src/utilty.py
@@ -5,3 +5,9 @@ import numpy as np
 def convert_image_to_nparray(file_name):
     return np.asarray(Image.open(file_name))
 
+
+def create_image_from_nparray(array, file_name, is_rgb):
+    if is_rgb:
+        Image.fromarray(array, "RGB").save(file_name)
+    else:
+        Image.fromarray(array, "L").save(file_name)
--- test/test.py
+++ test/test.py
@@ -1,11 +1,18 @@
 from src import utilty as util
+import numpy as np
 
 
 def test_open_image():
     image = util.convert_image_to_nparray("/home/utku/Documents/3.png")
     print("Image type: ", type(image))
     print("Image shape: ", image.shape)
+    return image
+
+
+def test_save_image(image):
+    util.create_image_from_nparray(image, "/home/utku/Documents/3_new.png", True)
 
 
 if __name__ == "__main__":
-    test_open_image()
+    img = test_open_image()
+    test_save_image(img)
