d1d7c96310bd23de3a8ebfe0dff5656d4ebf9c4c
Utku Anil Saykara <ge79quz@mytum.de>
2021-08-16 08:20:03 +0200

    Boundary find algorithm extended for diagonal cases

--- src/boundary.py
+++ src/boundary.py
@@ -9,14 +9,27 @@ def binary_matrix_to_boundaries(binary_map):
 
 
 def is_boundary(i, j, b_map):
-    if j + 1 < b_map.shape[1] and b_map[i][j + 1] == 1:
+    if j + 1 < b_map.shape[1]:
+        if b_map[i][j + 1] == 1:  # Checking Right
+            return True
+        elif i - 1 >= 0 and b_map[i - 1][j + 1] == 1:  # Checking Upper Right
+            return True
+        elif i + 1 < b_map.shape[0] and b_map[i + 1][j + 1] == 1:  # Checking Lower Right
+            return True
+
+    if j - 1 >= 0:
+        if b_map[i][j - 1] == 1:  # Checking Left
+            return True
+        elif i - 1 >= 0 and b_map[i - 1][j - 1] == 1:  # Checking Upper Left
+            return True
+        elif i + 1 < b_map.shape[0] and b_map[i + 1][j - 1] == 1:  # Checking Lower Left
+            return True
+
+    if i + 1 < b_map.shape[0] and b_map[i + 1][j] == 1:  # Checking Lower
         return True
-    elif j - 1 >= 0 and b_map[i][j - 1] == 1:
+
+    if i - 1 >= 0 and b_map[i - 1][j] == 1:  # Checking Upper
         return True
-    elif i + 1 < b_map.shape[0] and b_map[i + 1][j] == 1:
-        return True
-    elif i - 1 >= 0 and b_map[i - 1][j] == 1:
-        return True
-    else:
-        return False
+
+    return False
 
