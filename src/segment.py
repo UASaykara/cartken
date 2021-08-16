505a6c2f44cd963d23275921c23d59120ca0e230
Utku Anil Saykara <ge79quz@mytum.de>
2021-08-16 03:58:13 +0200

    Base structure of segment finder added

---
+++ src/segment.py
@@ -0,0 +1,22 @@
+class Segment:
+    def __init__(self, x, y, pose, width):
+        self.x = x
+        self.y = y
+        self.pose = pose
+        self.width = width
+
+
+def find_list_of_segment():
+    pass
+
+
+def find_longer_pose():
+    pass
+
+
+def extract_segment():
+    pass
+
+
+def check_equal_parallel_segment():
+    pass
