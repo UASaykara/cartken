6eb19b1836e0bc3c84efc5f90c8ff3d3bf7740e8
Utku Anil Saykara <ge79quz@mytum.de>
2021-08-16 08:00:51 +0200

    Fixed typo error in main and row alingment

--- src/main.py
+++ src/main.py
@@ -7,22 +7,26 @@ from datetime import datetime
 
 def main():
     image_path = input("Enter image name inside image folder with extension (Ex. image.jpg): ")
-    print("Boundaries are extracting...", end="\n")
     now = str(datetime.now())
+
+    print("Boundaries are extracting...", end="\n")
     os.mkdir(os.getcwd() + "/results/" + now)
     binary_map = util.rgb_to_binary(util.convert_image_to_nparray("/images/" + image_path))
     op.binary_matrix_to_boundaries(binary_map)
     util.create_image_from_nparray(util.binary_to_rgb(binary_map), "/results/" + now + "/boundary_map.png", True)
     print("Boundaries extracted!", end="\n")
+
     print("Segments are creating...", end="\n")
     segment_list = seg.find_list_of_segment(binary_map)
+
     util.segment_list_to_file(segment_list, "/results/" + now + "/segmentation_list")
     print("Segment text file created.", end="\n")
+
     rgb_map = util.construct_segment_map(segment_list, binary_map.shape[0], binary_map.shape[1])
     util.create_image_from_nparray(rgb_map, "/results/" + now + "/segmentation_map.png", True)
     print("Segmentation map created.", end="\n")
     print("Boundary segments created!", end="\n")
-    print("Results are inside [/results/" + now + " file.]" , end="\n")
+    print("Results are inside [/results/" + now + "] file." , end="\n")
 
 
 if __name__ == "__main__":
